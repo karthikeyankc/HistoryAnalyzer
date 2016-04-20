c441242707dcda646ad9c3e54c65d868df33a0f0
Karthikeyan KC <karthikeyankc@live.com>
2016-04-21 01:47:00 +0530

    Revisions and clean ups

--- HistoryAnalyzer.py
+++ HistoryAnalyzer.py
@@ -13,7 +13,23 @@ def parse(url):
 	except IndexError:
 		print "URL format error!"
 
-#path to user's history database
+def analyze(results):
+
+	prompt = raw_input("[.] Type <c> to print or <p> to plot\n[>] ")
+
+	if prompt == "c":
+		for site, count in sites_count_sorted.items():
+			print site, count
+	elif prompt == "p":
+		plt.bar(range(len(results)), results.values(), align='edge')
+		plt.xticks(rotation=45)
+		plt.xticks(range(len(results)), results.keys())
+		plt.show()
+	else:
+		print "[.] Uh?"
+		quit()
+
+#path to user's history database (Chrome)
 data_path = os.path.expanduser('~')+"\AppData\Local\Google\Chrome\User Data\Default"
 files = os.listdir(data_path)
 
@@ -36,15 +52,6 @@ for url, count in results:
 	else:
 		sites_count[url] = 1
 
-#sorting the dict by values
 sites_count_sorted = OrderedDict(sorted(sites_count.items(), key=operator.itemgetter(1), reverse=True))
 
-#results
-for site, count in sites_count_sorted.items():
-	print site, count
-
-#plotted results
-plt.bar(range(len(sites_count_sorted)), sites_count_sorted.values(), align='edge')
-plt.xticks(rotation=45)
-plt.xticks(range(len(sites_count_sorted)), sites_count_sorted.keys())
-plt.show()
+analyze (sites_count_sorted)
